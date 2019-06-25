d07749aeaf1d339ab70bb0af7f594e194e9d09ac
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-06-25 12:32:40 +0000

    fix test coverage [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- context_test.go
+++ context_test.go
@@ -133,6 +133,7 @@ func copyContext(in *lib.Ctx) *lib.Ctx {
 		TestMode:            in.TestMode,
 		ESBulkSize:          in.ESBulkSize,
 		HTTPTimeout:         in.HTTPTimeout,
+    CanReconnect:        in.CanReconnect,
 	}
 	return &out
 }
@@ -372,6 +373,7 @@ func TestInit(t *testing.T) {
 		TestMode:            true,
 		ESBulkSize:          10000,
 		HTTPTimeout:         2,
+    CanReconnect:        true,
 	}
 
 	var nilRegexp *regexp.Regexp
