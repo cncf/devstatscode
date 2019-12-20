2c7ac9ddf84148c65106eee01ada8cc0f363f124
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-12-20 12:43:30 +0000

    Detect error (that is allowed) and log it as a warning [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- cmd/calc_metric/calc_metric.go
+++ cmd/calc_metric/calc_metric.go
@@ -473,7 +473,11 @@ func handleSeriesDrop(ctx *lib.Ctx, con *sql.DB, cfg *calcMetricData) {
 					lib.Printf("Truncating table %s\n", table)
 				}
 				// lib.ExecSQLWithErr(con, ctx, "truncate "+table)
-				lib.ExecSQLWithErr(con, ctx, "drop table if exists "+table)
+				// lib.ExecSQLWithErr(con, ctx, "drop table if exists "+table)
+				_, err := lib.ExecSQL(con, ctx, "drop table "+table)
+				if err != nil {
+					lib.Printf("warning: failed dropping table '%s': %+v\n", table, err)
+				}
 			}
 		}
 	}
