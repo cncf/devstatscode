5e085b24c71fcfb85e30c04969bda983a11574d4
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2020-02-26 15:42:31 +0000

    example updated [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- devel/api_dev_act_cnt_example.sh
+++ devel/api_dev_act_cnt_example.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-./devel/api_dev_act_cnt_repo_grp.sh kubernetes 'Last year' 'GitHub Events' 'SIG Apps' 'United States' 'idvoretskyi'
+API_URL='https://teststats.cncf.io/api/v1' ./devel/api_dev_act_cnt_repo_grp.sh kubernetes 'v1.17.0 - now' 'GitHub Events' 'SIG Apps' 'United States' 'janetkuo'
+API_URL='https://devstats.cncf.io/api/v1' ./devel/api_dev_act_cnt_repo_grp.sh kubernetes 'v1.17.0 - now' 'GitHub Events' 'SIG Apps' 'United States' ''
