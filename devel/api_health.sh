ef341261c7987a5c41f64ca0fe02422b4bd62222
Sameer Vohra <vohra.sam@gmail.com>
2021-01-04 10:50:21 -0600

    Update api_health.sh
    Fix typo

--- devel/api_health.sh
+++ devel/api_health.sh
@@ -9,7 +9,7 @@ then
   API_URL="http://127.0.0.1:8080/api/v1"
 fi
 project="${1}"
-echo 'Note that last API c all should succeed and all other should fail due to expected errors'
+echo 'Note that last API call should succeed and all other should fail due to expected errors'
 curl http://127.0.0.1:8080/api/v1 -d"xyz" 2>/dev/null | jq
 curl -H "Content-Type: application/json" "${API_URL}" -d"{\"api\":\"health\",\"payload\":{\"project\":\"${project}\"}}" 2>/dev/null | jq
 curl -H "Content-Type: application/json" "${API_URL}" -d"{\"api\":\"Health\",\"payloada\":{\"project\":\"${project}\"}}" 2>/dev/null | jq
