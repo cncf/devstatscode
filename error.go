dc817f11334e63112fb6b3898447b3274a2c3fb8
Łukasz Gryglicki <lgryglicki@cncf.io>
2021-10-15 07:57:44 +0000

    Non-recoverable errors with DURABLE_PQ
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

--- error.go
+++ error.go
@@ -29,8 +29,13 @@ func FatalOnError(err error) string {
 			Printf("PqError: code=%s, name=%s, detail=%s\n", e.Code, errName, e.Detail)
 			fmt.Fprintf(os.Stderr, "PqError: code=%s, name=%s, detail=%s\n", e.Code, errName, e.Detail)
 			if os.Getenv("DURABLE_PQ") != "" && os.Getenv("DURABLE_PQ") != "0" && os.Getenv("DURABLE_PQ") != "false" {
-				Printf("retrying with DURABLE_PQ\n")
-				return Reconnect
+				switch errName {
+				case "program_limit_exceeded":
+					Printf("%s error is not retryable, even with DURABLE_PQ\n", errName)
+				default:
+					Printf("retrying with DURABLE_PQ\n")
+					return Reconnect
+				}
 			}
 		default:
 			Printf("ErrorType: %T, error: %+v\n", e, e)
