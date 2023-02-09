fc3f20252839e6add1f607b6ace4577e5393e812
Łukasz Gryglicki <lgryglicki@cncf.io>
2023-02-09 13:50:10 +0000

    One more fix
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

--- cmd/gha2db/gha2db.go
+++ cmd/gha2db/gha2db.go
@@ -2109,7 +2109,7 @@ func refreshCommitRoles(ctx *lib.Ctx) {
 		rows := lib.QuerySQLWithErr(
 			con,
 			ctx,
-			fmt.Sprintf("select sha, event_id, dup_repo_id, dup_repo_name, dup_created_at, message "+
+			fmt.Sprintf("select distinct sha, event_id, dup_repo_id, dup_repo_name, dup_created_at, message "+
 				"from gha_commits where (sha, event_id) not in (select sha, event_id from gha_commits_roles) "+
 				"limit %d offset %d",
 				limit,
