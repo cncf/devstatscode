c685d55ef6ab850ff5e0765f7c866a4974d159da
Łukasz Gryglicki <lgryglicki@cncf.io>
2024-09-06 06:19:45 +0000

    Test a new regexp
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

--- regexp_test.go
+++ regexp_test.go
@@ -261,6 +261,10 @@ func TestAnnotationRegexp(t *testing.T) {
 		{re: `^v\d+\.\d+\.0(-beta\.\d*0)?$`, str: "v1.0.0-beta.10", match: true},
 		{re: `^v\d+\.\d+\.0(-beta\.\d*0)?$`, str: "v1.0.0-beta.1", match: false},
 		{re: `^v\d+\.\d+\.0(-beta\.\d*0)?$`, str: "v1.0.0-beta.101", match: false},
+		{re: `^v\d+\.\d+\.\d*0$`, str: "v1.2.0", match: true},
+		{re: `^v\d+\.\d+\.\d*0$`, str: "v1.2.10", match: true},
+		{re: `^v\d+\.\d+\.\d*0$`, str: "v1.2.110", match: true},
+		{re: `^v\d+\.\d+\.\d*0$`, str: "v1.2.1", match: false},
 	}
 	// Execute test cases
 	for index, test := range testCases {
